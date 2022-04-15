b8b51e7aee6e2b1941bd134c4d411951cac3a752
YOHAN <jkim6115@gmail.com>
2022-04-15 20:42:15 +0900

    baekjoon 2438

--- src/Package1/Main.java
+++ src/Package1/Main.java
@@ -2,7 +2,7 @@ package Package1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+//import java.util.StringTokenizer;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,18 +11,15 @@ public class Main{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter pw = new PrintWriter(System.out);
-		StringTokenizer st;
+//		StringTokenizer st;
 		
-		int T = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		for(int count=1; count<=T; count++) {
-			
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			pw.printf("Case #%d: " + (A+B), count);
-			pw.print("\n");
-			
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				pw.write("*");
+			}
+			pw.write("\n");
 		}
 		
 		br.close();
